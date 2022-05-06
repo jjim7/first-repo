1605e2cd8581df7e885c42b32a00bfc1ddad08d3
haeun choi <haeun705@gmail.com>
2022-05-06 13:29:35 +0900

    Print 'yeardream' when it is times of 3,5,15

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 print('hello')
 
 for i in range(1,21):
-    if i / 3 == 0:
+    if i % 3 == 0:
         print("year")
-    elif i / 5 == 0:
+    elif i % 5 == 0:
         print("dream")
-    elif i / 15 == 0:
+    elif i % 15 == 0:
         print("yeardream")
 
     else:
