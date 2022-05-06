964ede1862cb79ef61cafa8c3932e05d0e24d923
haeun choi <haeun705@gmail.com>
2022-05-06 13:23:11 +0900

    Python practice
    print yeardream when i is 3,5,15

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 print('hello')
 
-for i in range(1,11):
+for i in range(1,21):
     if i / 3 == 0:
         print("year")
+    elif i / 5 == 0:
+        print("dream")
+    elif i / 15 == 0:
+        print("yeardream")
 
     else print(i)
-
 
 
 
